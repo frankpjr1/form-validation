ec62eb40e3d2835a72c84a2fdc7a58381267a04e
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-01-12 12:55:28 -0500

    commit added new element input with type checkbox, deleted customer number  input

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+  const clientName = document.querySelector('#client-name');
+console.log(clientName)
+  // const clientPhone = document.getElementById('phone');
+  // const clientEmail = document.getElementById('email');
+  // const clientServices = document.getElementById('services');
+  // const testMessage = document.getElementById('test-message');
+  // const formSubmit = document.getElementById('submit');
+
+
