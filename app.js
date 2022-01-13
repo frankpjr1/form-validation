80b6fb82dde9bf030139c2e83952e470b7753e10
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-01-13 05:17:49 -0500

    commit added logic to app.js file to validate information entered to the form

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 
+  const myForm = document.getElementById('project-form');
 
-  const clientName = document.querySelector('#client-name');
-console.log(clientName)
+  // const clientName = document.getElementById('name');
   // const clientPhone = document.getElementById('phone');
-  // const clientEmail = document.getElementById('email');
-  // const clientServices = document.getElementById('services');
-  // const testMessage = document.getElementById('test-message');
-  // const formSubmit = document.getElementById('submit');
+
+    const submit = document.getElementById(('submit'))
+  const clientName = document.getElementById('name');
+  const clientPhone = document.getElementById('phone');
+  const clientEmail = document.getElementById('email');
+  const clientServices = document.getElementById('services');
+  const clientMessages = document.getElementById('client_message');
+  const clientTerms = document.getElementById('terms-conditions');
+
+  const error = []
+  submit.addEventListener('click',(e)=>{
+      e.preventDefault()
+
+        if (clientName.value === "" || clientName.value === null){
+            prompt("enter name")
+        }
+
+        if (clientPhone.value === "" || clientPhone.value === null){
+            prompt("enter a phone number")
+        }
+
+        if (clientEmail.value === "" || clientEmail.value === null){
+          prompt("enter your email")
+        }
+
+
+
+        if (clientMessages.value === "" || clientMessages.value === null){
+          prompt("please write us a short note")
+        }
+
+        if (clientTerms.value === null){
+            prompt("check our terms")
+        }
+        else{
+
+        }
+
+
+  })
+
+
+
+
+
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
       fontFamily:{
         body:['Montserrat']
       }
